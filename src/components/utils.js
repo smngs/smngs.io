de37fc6ec6624886f007a53cd580795a66861d7f
Shota Minegishi <smngs@mine-313.com>
2025-11-10 15:00:50 +0900

    Revert "Improve formatting of Astro layout and components"

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,25 +1,25 @@
 export function formatToMonthYear(dateStr) {
   const date = new Date(dateStr);
-  const parts = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "short",
+  const parts = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
   }).formatToParts(date);
 
-  const month = parts.find((part) => part.type === "month")?.value;
-  const year = parts.find((part) => part.type === "year")?.value;
+  const month = parts.find(p => p.type === 'month')?.value;
+  const year  = parts.find(p => p.type === 'year')?.value;
 
   return `${month}. ${year}`;
 }
 
 export function formatToMonthYearJP(dateStr) {
   const date = new Date(dateStr);
-  const parts = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "numeric",
+  const parts = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'numeric',
   }).formatToParts(date);
 
-  const month = parts.find((part) => part.type === "month")?.value;
-  const year = parts.find((part) => part.type === "year")?.value;
+  const month = parts.find(p => p.type === 'month')?.value;
+  const year  = parts.find(p => p.type === 'year')?.value;
 
   return `${year} 年 ${month} 月`;
 }
