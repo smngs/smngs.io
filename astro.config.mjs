457b95d0a3e3198ac8368df5e742bd4a26434dda
Shota Minegishi <smngs@smngs.io>
2025-11-10 05:26:25 +0000

    [update]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://smngs.io',
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
