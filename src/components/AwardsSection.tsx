16ff2d00e271f24dafcb37ef0cb7b0229b6b1c04
Shota Minegishi <smngs@smngs.io>
2026-05-24 16:05:30 +0900

    [update] replace press release text link with newspaper icon

--- src/components/AwardsSection.tsx
+++ src/components/AwardsSection.tsx
@@ -1,5 +1,7 @@
 import awards from "../../data/award.json";
 import { formatToMonthYear, formatToMonthYearJP } from "@/lib/format";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
 
 export function AwardsSection() {
   return (
@@ -17,18 +19,15 @@ export function AwardsSection() {
             {award.lang === "ja"
               ? formatToMonthYearJP(award.date)
               : formatToMonthYear(award.date)}
-            {award.press_url ? (
-              award.lang === "ja" ? (
-                <span>
-                  , <a href={award.press_url}>所属機関発表</a>.
-                </span>
-              ) : (
-                <span>
-                  , <a href={award.press_url}>Press Release</a>.
-                </span>
-              )
-            ) : (
-              <>.</>
+            .
+            {award.press_url && (
+              <a
+                href={award.press_url}
+                className="reference-icon"
+                aria-label={award.lang === "ja" ? "所属機関発表" : "Press Release"}
+              >
+                <FontAwesomeIcon icon={faNewspaper} />
+              </a>
             )}
           </li>
         ))}
